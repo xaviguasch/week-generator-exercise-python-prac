049b1e747151363876d7e496c782135d4304a331
xaviguasch <xaviguasch@gmail.com>
2021-01-27 12:16:09 +0100

    added finished exercise

---
+++ main.py
@@ -0,0 +1,31 @@
+'''
+days = week()
+next(days) # 'Monday'
+next(days) # 'Tuesday'
+next(days) # 'Wednesday'
+next(days) # 'Thursday'
+next(days) # 'Friday'
+next(days) # 'Saturday'
+next(days) # 'Sunday'
+next(days) # StopIteration
+'''
+
+
+def week():
+    days = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+    ]
+    for day in days:
+        yield day
+
+
+counter = week()
+
+for num in counter:
+    print(num)
